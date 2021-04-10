872f0bf53b99919c6c75c01a423471b5081602de
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-04-10 16:44:12 +0530

    Added: Check attendance using method

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,1 +1,20 @@
 console.log("Welcome to Employee wage program");
+const isAbsent = 0;
+let empCheck = Math.floor(Math.random()*10) %2;
+class Employee{
+    constructor(){};
+    
+    attenfance(){
+        if(empCheck == isAbsent)
+        {
+            console.log("Employee is Absent");
+        }
+        else{
+            console.log("Employee is present");
+        }
+        return empCheck;
+    }
+}
+
+let emp = new Employee().attenfance();
+console.log(emp);
