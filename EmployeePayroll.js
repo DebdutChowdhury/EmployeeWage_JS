34e5a353ccaa051c7262563af35bc69e2ca214a1
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-04-10 17:55:46 +0530

    Added: Here we use full and part time using switch case

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,27 +1,37 @@
 console.log("Welcome to Employee wage program");
-const wagePrHr = 20;
-const hrInDay = 8;
-let empCheck = Math.floor(Math.random()*10) %2;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOUR = 4;
+const FULL_TIME_HOUR = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHr = 0;
+let empCheck = Math.floor(Math.random()*10) %3;
 
 class Employee{
     constructor(){};
     
     attenfance(){
-        if(empCheck == 1)
-        {
-            console.log("Employee is Absent");
-        }
-        else{
-            console.log("Employee is present");
+        switch(empCheck){
+            case IS_FULL_TIME : 
+                empHr = FULL_TIME_HOUR;
+                break;
+            case IS_PART_TIME : 
+                empHr = PART_TIME_HOUR;
+                break;
+            default:
+                empHr = 0;
         }
         return empCheck;
     }
 
     checkDaillyWage(){
-        let daillyWage = wagePrHr * hrInDay * empCheck;
-        return daillyWage;
+        let empWage = empHr * WAGE_PER_HOUR;
+        return empWage;
     }
 }
 
-let emp = new Employee().checkDaillyWage();
+let emp = new Employee().attenfance();
+let daillywage = new Employee().checkDaillyWage();
 console.log(emp);
+console.log(`Emp Wage: ${daillywage}`);
