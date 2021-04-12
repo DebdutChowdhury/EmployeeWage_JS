64e7f0fcd2a641628255299705c8c67e46767170
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-04-12 09:32:41 +0530

    Added: Total working days and hrs run conditional till 100 hrs and 20 days

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,35 +1,51 @@
 console.log("Welcome to Employee wage program");
-const WAGE_PER_HR = 20;
-const IS_FULL_TIME = 1;
-const IS_PART_TIME = 2;
-const TOTAL_MONTHLY_DAYS = 20;
 
-let empHr;
-let monthlyWage = 0;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOUR = 4;
+const FULL_TIME_HOUR = 8;
+const WAGE_PER_HOUR = 20;
+const WORKING_HOURS = 20;
+const MAX_HR_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+const IS_PART_TIME = 1;
 
-class Employee{
-    constructor(){};
-    
-    attenfance(){
-        for (let day = 0; day < TOTAL_MONTHLY_DAYS; day++){
-            let empCheck = Math.floor(Math.random()*3);
-            switch(empCheck){
-                case IS_FULL_TIME : 
-                    empHr = 8;
-                    break;
-                case IS_PART_TIME : 
-                    empHr = 4;
-                    break;
-                default:
-                    empHr = 0;
-                    break;
-            }
-            let daillyWage = empHr * WAGE_PER_HR;
-            monthlyWage = monthlyWage + daillyWage; 
+let empHr = 0;
+
+class Employee {
+    constructor() { };  
+
+
+    attendance(empCheck) {
+        switch (empCheck) {
+            case this.IS_PART_TIME:
+                empHr = this.PART_TIME_HOUR;
+            case this.IS_FULL_TIME:
+                empHr = this.FULL_TIME_HOUR;
+            default:
+                empHr = 0;
         }
-        return monthlyWage;
+        return empCheck;
+    }
+
+    daillyWage(empHr) {
+        return empHr * WAGE_PER_HOUR;
+    }
+
+
+
+    checkWage() {
+        let totalEmpHrs = 0;
+        let totalWorkingDays = 0;
+        let empDailyWAgeArr = new Array();
+        while (totalEmpHrs <= MAX_HR_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            let empCheck = Math.floor(Math.random() * 10) % 3;
+            totalEmpHrs += this.attendance(empCheck);
+            empDailyWAgeArr.push(this.daillyWage(totalEmpHrs));
+        }
+        let empWage = new Employee().daillyWage(totalEmpHrs);
+        console.log("Total Days: " + totalWorkingDays + ", Employee Hours: " + totalEmpHrs + ", Emp Wage: " + empWage);
     }
 }
 
-let emp = new Employee().attenfance();
-console.log(`Montly Wage: ${emp}`);
+let reault = new Employee().checkWage();
